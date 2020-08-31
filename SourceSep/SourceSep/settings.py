d176eab3df059535f587d10abd2ea66ff0d05c59
Ciaran O'Connor <c16354461@mydit.ie>
2020-08-31 12:51:19 +0100

    Update settings.py

--- SourceSep/SourceSep/settings.py
+++ SourceSep/SourceSep/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a_!^f460moxu%h*%+%e9ci&_r&(-q_*r+id$u(-m0q0s2-=*__'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
